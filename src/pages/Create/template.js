a9a2e78c257d8ca7e19fe68b43ed3dbf7bb72004
wangliuyong <1355498705@qq.com>
2018-09-15 20:20:11 +0800

    创建页面

--- src/pages/Create/template.js
+++ src/pages/Create/template.js
@@ -1,7 +1,21 @@
+import blog from '@/api/blog.js'
+
 export default {
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title:'',
+      description:'',
+      content:'',
+      atIndex:''
     }
+  },
+  methods:{
+    onCreate(){
+      blog.createBlog({title:this.title,description:this.description,content:this.content,atIndex:this.atIndex})
+         .then((res)=>{
+           this.$message.success(res.msg)
+           this.$router.push({path:`/detail/${res.data.id}`})
+         })
+    },
   }
 }

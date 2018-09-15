7dcaeb3601aa1b5ec0dabc9c7fd18d9a44b51032
wangliuyong <1355498705@qq.com>
2018-09-15 22:21:08 +0800

    主页面获取博客列表

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -2,32 +2,37 @@ import request from '@/helpers/request.js'
 import auth from '@/api/auth.js'
 import blog from '@/api/blog.js'
 
-window.request = request
+/* window.request = request
 window.auth = auth
-window.blog = blog
-
-
-
+window.blog = blog */
 
 export default {
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      blogs:[],
+      total:0,
+      page:1
     }
   },
-
+  created() {
+    
+    this.page=parseInt(this.$route.query.page)||1
+    blog.getIndexBlogs({page:this.page}).then((res)=>{
+      this.blogs=res.data
+      this.total=res.total
+      this.page=res.page
+    })
+  },
   methods: {
-    onClick1() {
-      this.$message.error('错了哦，这是一条错误消息')
+    handleCurrentChange(page){
+      blog.getIndexBlogs({page:page}).then((res)=>{
+        this.blogs=res.data
+        this.total=res.total
+        this.page=res.page
+        this.$router.push({path:'/',query:{page:page}})
+        console.log(res)
+        
+      })
     },
-
-    onClick2() {
-      this.$alert('这是一段内容', '标题名称', {
-          confirmButtonText: '确定',
-          callback: action => {
-            this.$message.success('点了确定')
-          }
-        })
-    }
   }
 }
--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="index">
-    <section class="blog-posts">
+    <section class="blog-posts" v-for="(item,index) in blogs" :key="index">
       <div class="item">
         <figure class="avatar">
-          <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="">
-          <figcaption>若愚</figcaption> 
+          <img :src="item.user.avatar" :alt="item.user.username">
+          <figcaption>{{item.user.username}}</figcaption> 
         </figure>
-        <h3>前端异步大揭秘 <span>3天前</span></h3> 
+        <h3>{{item.title}}<span>{{item.createdAt}}</span></h3> 
         <p>本文以一个简单的文件读写为例，讲解了异步的不同写法，包括 普通的 callback、ES2016中的Promise和Generator、 Node 用于解决回调的co 模块、ES2017中的async/await。适合初步接触 Node.js以及少量 ES6语法的同学阅读...</p>
+        <router-link :to="`/detail?${item.id}`" class="link">详情</router-link>
       </div>
-      <div class="item">
-        <figure class="avatar">
-          <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="">
-          <figcaption>若愚</figcaption> 
-        </figure>
-        <h3>前端异步大揭秘 <span>3天前</span></h3> 
-        <p>本文以一个简单的文件读写为例，讲解了异步的不同写法，包括 普通的 callback、ES2016中的Promise和Generator、 Node 用于解决回调的co 模块、ES2017中的async/await。适合初步接触 Node.js以及少量 ES6语法的同学阅读...</p>
+      <hr/>
+    </section>
+    <section>
+      <div class="block">
+        <el-pagination
+        layout="prev, pager, next"
+        :total="total"
+        @current-change="handleCurrentChange">
+        </el-pagination>
       </div>
     </section>
+    
   </div>
 </template>
 

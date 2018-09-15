b2e7449b2e7d0593b8edfdded71b5a406155e5a3
wangliuyong <1355498705@qq.com>
2018-09-15 17:36:36 +0800

    完善logo、pen链接

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,63 +1,61 @@
 <template>
   <header :class="{login: isLogin, 'no-login': !isLogin}">
     <template v-if="!isLogin">
-      <h1>Let's share</h1>
+      <router-link to="/"><h1>Let's share</h1></router-link>
       <p>精品博客汇聚</p>
       <div class="btns">
         <router-link to="/login"><el-button >立即登录</el-button></router-link>
         <router-link to="/register"><el-button>注册账号</el-button></router-link>
-        
       </div> 
     </template>
     <template v-if="isLogin">
-      <h1>Let's share</h1>
-      <i class="edit el-icon-edit"></i>
-      <div class="user">
-        <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username">
-        <ul>
-          <li><router-link to="my">my</router-link></li>
-          <li><a href="#" @click="onLogout">注销</a></li>
-        </ul>
+      <router-link to="/"><h1>Let's share</h1></router-link>
+      <div class="wrap">
+        <router-link to="/create"><i class="edit el-icon-edit"></i></router-link>
+        <div class="user">
+          <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username">
+          <ul>
+            <li><router-link to="my">my</router-link></li>
+            <li><a href="#" @click="onLogout">注销</a></li>
+          </ul>
+        </div>
       </div>
+      
                 
     </template>
   </header>
 </template>
 
 <script>
-  import {mapState,mapGetters,mapMutations,mapActions} from 'vuex'
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 
-  import auth from '@/api/auth.js'
-  export default {
-    data() {
-      return {}
-    },
-    computed:{
-      ...mapGetters([
-        'user',
-        'isLogin'
-        ]),
-    },
-    created(){
-      this.checkLogin()
-    },
-    methods:{
-      ...mapActions([
-        'checkLogin',
-        'logout'
-      ]),
-      onLogout(){
-        this.logout()
-      },
-    },
-    
+import auth from "@/api/auth.js";
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["user", "isLogin"])
+  },
+  created() {
+    this.checkLogin();
+  },
+  methods: {
+    ...mapActions(["checkLogin", "logout"]),
+    onLogout() {
+      this.logout();
+    }
   }
+};
 </script>
 
 
 <style lang="less">
-ul{margin: 0;padding: 0;}
-@import "../assets/base.less";  
+ul {
+  margin: 0;
+  padding: 0;
+}
+@import "../assets/base.less";
 
 header.no-login {
   padding: 0 12% 30px 12%;
@@ -76,7 +74,7 @@ header.no-login {
     margin: 15px 0 0 0;
     color: #fff;
   }
-  
+
   .btns {
     margin-top: 20px;
   }
@@ -85,14 +83,13 @@ header.no-login {
     margin: 20px 5px 0;
     background: #fff;
   }
-} 
-
+}
 
 header.login {
   display: flex;
   align-items: center;
   background: @bgColor;
-
+  justify-content: space-between;
   h1 {
     margin: 0;
     padding: 0;
@@ -106,37 +103,43 @@ header.login {
     color: #fff;
     font-size: 30px;
   }
-  .user{
+  .wrap{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    }
+  .user {
     position: relative;
     
     .avatar {
-    width: 40px;
-    height: 40px;
-    border: 1px solid #fff;
-    border-radius: 50%;
-    margin-left: 15px;
+      width: 40px;
+      height: 40px;
+      border: 1px solid #fff;
+      border-radius: 50%;
+      margin-left: 15px;
     }
-    ul{
+    ul {
       display: none;
-      position:absolute;
+      position: absolute;
       top: 42px;
       left: 14px;
       list-style: none;
-      background: #FFF;
+      background: #fff;
       width: 80%;
-      li{text-align: center;padding: 3px 5px;}
-      li:hover{background: #ccc;}
-      a{text-decoration: none;}
-      
+      li {
+        text-align: center;
+        padding: 3px 5px;
+      }
+      li:hover {
+        background: #ccc;
+      }
+      a {
+        text-decoration: none;
+      }
     }
-    &:hover>ul{display: block;}
+    &:hover > ul {
+      display: block;
+    }
   }
-  
-  
-
-  
 }
-
-
-
 </style>
